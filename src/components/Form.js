0cf2513ff25baf99131f7f4bd99578ab1a7740c4
Joe Keohan <jkeohan@gmail.com>
2023-05-01 19:30:52 -0400

    inital setup of Form & MovieDisplay components

---
+++ src/components/Form.js
@@ -0,0 +1,5 @@
+const Form = (props) => {
+    return <h2>Form Component </h2>
+}
+
+export default Form;
---
+++ src/components/MovieDisplay.js
@@ -0,0 +1,5 @@
+const MovieDisplay = (props) => {
+	return <h1>The MovieDisplay Component</h1>;
+};
+
+export default MovieDisplay;
